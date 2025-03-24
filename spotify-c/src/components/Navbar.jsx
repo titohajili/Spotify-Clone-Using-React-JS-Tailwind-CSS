e573118f538146cdfa2d3e2d97fc5f8d905efe37
titohajili <maqatoto963@gmail.com>
2025-03-24 23:48:40 +0400

    Create Album List and Song List

--- spotify-c/src/components/Navbar.jsx
+++ spotify-c/src/components/Navbar.jsx
@@ -12,7 +12,13 @@ const Navbar = () => {
         <div className='flex items-center gap-4'>
             <p className='bg-white text-black text-[15px] px-4 py-1 rounded-2xl hidden md:block cursor-pointer'>Explore Premium</p>
             <p className='bg-black py-1 px-3 rounded-2xl text-[15px] cursor-pointer'>Install App</p>
+            <p className='bg-purple-500 text-black w-7 h-7 rounded-full flex items-center justify-center'>T</p>
         </div>
+      </div>
+      <div className='items-center flex gap-2 mt-4'>
+          <p className='bg-white text-black px-4 py-1 rounded-2xl cursor-pointer'>All</p>
+          <p className='bg-black px-4 py-1 rounded-2xl cursor-pointer'>Music</p>
+          <p className='bg-black px-4 py-1 rounded-2xl cursor-pointer'>Podcasts</p>
       </div>
     </>
   )
