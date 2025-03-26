9e6735a0602e587d0b0f9686de8e463f01963496
titohajili <maqatoto963@gmail.com>
2025-03-26 23:55:42 +0400

    Create Music Album Page

---
+++ spotify-c/src/components/DisplayAlbum.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import Navbar from './Navbar'
+import { useParams } from 'react-router-dom'
+import { albumsData, assets } from '../assets/assets'
+
+const DisplayAlbum = () => {
+
+    const {id} = useParams()
+    const albumData = albumsData[id]    
+    
+    
+  return (
+    <>
+      <Navbar/>
+      <div className='mt-10 flex gap-8 flex-col md:flex-row md:items-end'>
+        <img className='w-48 rounded' src={albumData.image} alt="" />
+        <div className='flex flex-col'>
+            <p>Playlist</p>
+            <h2 className='text-5xl font-bold mb-4 md:text-7xl'>{albumData.name}</h2>
+            <h4>{albumData.desc}</h4>
+            <p className='mt-1'>
+                <img className='inline-block w-5 ' src={assets.spotify_logo} alt="" />
+                <b>Spotify</b>
+                * 1,233,154 likes
+                * <b>50 songs,</b>
+                bout 2 hr 30 min
+            </p>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default DisplayAlbum
