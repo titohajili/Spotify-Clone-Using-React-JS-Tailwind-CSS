f3af229340d17ba1c9e5893fa01aee74f3bf729d
titohajili <maqatoto963@gmail.com>
2025-03-27 23:47:32 +0400

    Create Music Album Page

--- spotify-c/src/components/DisplayAlbum.jsx
+++ spotify-c/src/components/DisplayAlbum.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Navbar from './Navbar'
 import { useParams } from 'react-router-dom'
-import { albumsData, assets } from '../assets/assets'
+import { albumsData, assets, songsData } from '../assets/assets'
 
 const DisplayAlbum = () => {
 
@@ -22,11 +22,32 @@ const DisplayAlbum = () => {
                 <img className='inline-block w-5 ' src={assets.spotify_logo} alt="" />
                 <b>Spotify</b>
                 * 1,233,154 likes
-                * <b>50 songs,</b>
-                bout 2 hr 30 min
+                * <b>50 songs, </b>
+                bout 2 hr 30 minc
             </p>
         </div>
       </div>
+      <div className='grid grid-cols-3 sm:grid-cols-4 mt-10 mb-4 ol-2 text-[#a7a7a7]'> 
+        <p><b className='mr-4'>#</b>Title</p>
+        <p>Album</p>
+        <p className='hodden sm:block'>Date Added</p>
+        <img className='m-auto w-4' src={assets.clock_icon} alt="" />
+      </div>
+      <hr />
+      {
+        songsData.map((item, index)=>(
+            <div key={index} className='grid grid-cols-3 sm:grid-cols-4 gap-2 p-2 items-center text-[#a7a7a7] hover:bg-[#ffffff2b] cursor-pointer'>
+                <p className='text-white'>
+                  <b className='mr-4 text-[#a7a7a7]'>{index+1}</b>
+                  <img className='inline w-10 mr-5' src={item.image} alt="" />
+                  {item.name}
+                </p>
+                <p className='text-[15px]'>{albumData.name}</p>
+                <p className='text-[15px] hidden sm:block'>5 days ago</p>
+                <p className='text-[15px] text-center'>{item.duration}</p>
+            </div>
+        ))
+      }
     </>
   )
 }
